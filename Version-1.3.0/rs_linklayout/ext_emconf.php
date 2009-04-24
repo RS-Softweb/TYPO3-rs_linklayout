9ea5469c0085749b4891fbde5645ebbcd742bcf8
renestaeker <renestaeker@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-24 18:20:24 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rs_linklayout/trunk@19443 735d13b6-9817-0410-8766-e36946ffe9aa

--- Version-1.3.0/rs_linklayout/ext_emconf.php
+++ Version-1.3.0/rs_linklayout/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "rs_linklayout"
 #
-# Auto generated 07-12-2008 00:05
+# Auto generated 24-04-2009 20:01
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'shy' => 0,
 	'version' => '1.3.0',
 	'dependencies' => '',
-	'conflicts' => '',
+	'conflicts' => 'dh_linklayout',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -45,7 +45,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:69:{s:25:"class.tx_rslinklayout.php";s:4:"74db";s:12:"ext_icon.gif";s:4:"b751";s:28:"ext_typoscript_constants.txt";s:4:"c406";s:24:"ext_typoscript_setup.txt";s:4:"b847";s:14:"doc/manual.sxw";s:4:"3e44";s:11:"res/3ds.gif";s:4:"122b";s:15:"res/CREDITS.txt";s:4:"d42e";s:13:"res/Thumbs.db";s:4:"8ce1";s:10:"res/ai.gif";s:4:"2cb7";s:11:"res/ani.gif";s:4:"3d5f";s:10:"res/au.gif";s:4:"0199";s:11:"res/avi.gif";s:4:"27bd";s:11:"res/bmp.gif";s:4:"a7a6";s:11:"res/cdr.gif";s:4:"6fc9";s:11:"res/css.gif";s:4:"4786";s:11:"res/csv.gif";s:4:"e413";s:15:"res/default.gif";s:4:"ec6e";s:11:"res/doc.gif";s:4:"8c62";s:11:"res/dtd.gif";s:4:"48e2";s:11:"res/eps.gif";s:4:"4262";s:11:"res/exe.gif";s:4:"e703";s:11:"res/fh3.gif";s:4:"b429";s:13:"res/flash.gif";s:4:"0ab7";s:14:"res/folder.gif";s:4:"f3ad";s:11:"res/gif.gif";s:4:"1559";s:11:"res/htm.gif";s:4:"54de";s:12:"res/html.gif";s:4:"3cea";s:13:"res/html1.gif";s:4:"fdc5";s:13:"res/html2.gif";s:4:"ae09";s:13:"res/html3.gif";s:4:"23ae";s:11:"res/ico.gif";s:4:"6756";s:11:"res/inc.gif";s:4:"57e4";s:12:"res/java.gif";s:4:"52de";s:11:"res/jpg.gif";s:4:"23ac";s:10:"res/js.gif";s:4:"7a5a";s:16:"res/link_ext.gif";s:4:"03a6";s:16:"res/link_int.gif";s:4:"6e51";s:19:"res/link_mailto.gif";s:4:"c381";s:11:"res/max.gif";s:4:"d6f4";s:11:"res/mid.gif";s:4:"0d95";s:11:"res/mov.gif";s:4:"d5e6";s:11:"res/mp3.gif";s:4:"b37e";s:12:"res/mpeg.gif";s:4:"15b5";s:11:"res/mpg.gif";s:4:"15b5";s:11:"res/pcd.gif";s:4:"e9f3";s:11:"res/pcx.gif";s:4:"7d29";s:11:"res/pdf.gif";s:4:"5c5f";s:12:"res/php3.gif";s:4:"e58b";s:11:"res/png.gif";s:4:"fffc";s:10:"res/ps.gif";s:4:"0b48";s:11:"res/psd.gif";s:4:"4448";s:11:"res/rtf.gif";s:4:"f660";s:12:"res/sgml.gif";s:4:"13e6";s:11:"res/swf.gif";s:4:"d44f";s:11:"res/sxc.gif";s:4:"a29e";s:11:"res/sxw.gif";s:4:"2bc9";s:11:"res/t3d.gif";s:4:"9a71";s:11:"res/t3x.gif";s:4:"558e";s:11:"res/tga.gif";s:4:"20fc";s:11:"res/tif.gif";s:4:"533b";s:12:"res/tmpl.gif";s:4:"5114";s:11:"res/ttf.gif";s:4:"9f93";s:11:"res/txt.gif";s:4:"d7f9";s:11:"res/wav.gif";s:4:"6931";s:11:"res/wrl.gif";s:4:"132d";s:11:"res/xls.gif";s:4:"4a22";s:11:"res/xml.gif";s:4:"2e7b";s:11:"res/xsl.gif";s:4:"2a99";s:11:"res/zip.gif";s:4:"5de4";}',
+	'_md5_values_when_last_written' => 'a:67:{s:25:"class.tx_rslinklayout.php";s:4:"74db";s:12:"ext_icon.gif";s:4:"b751";s:28:"ext_typoscript_constants.txt";s:4:"c406";s:24:"ext_typoscript_setup.txt";s:4:"b847";s:14:"doc/manual.sxw";s:4:"3e44";s:11:"res/3ds.gif";s:4:"122b";s:15:"res/CREDITS.txt";s:4:"d42e";s:10:"res/ai.gif";s:4:"2cb7";s:11:"res/ani.gif";s:4:"3d5f";s:10:"res/au.gif";s:4:"0199";s:11:"res/avi.gif";s:4:"27bd";s:11:"res/bmp.gif";s:4:"a7a6";s:11:"res/cdr.gif";s:4:"6fc9";s:11:"res/css.gif";s:4:"4786";s:11:"res/csv.gif";s:4:"e413";s:15:"res/default.gif";s:4:"ec6e";s:11:"res/doc.gif";s:4:"8c62";s:11:"res/dtd.gif";s:4:"48e2";s:11:"res/eps.gif";s:4:"4262";s:11:"res/exe.gif";s:4:"e703";s:11:"res/fh3.gif";s:4:"b429";s:13:"res/flash.gif";s:4:"0ab7";s:11:"res/gif.gif";s:4:"1559";s:11:"res/htm.gif";s:4:"54de";s:12:"res/html.gif";s:4:"3cea";s:13:"res/html1.gif";s:4:"fdc5";s:13:"res/html2.gif";s:4:"ae09";s:13:"res/html3.gif";s:4:"23ae";s:11:"res/ico.gif";s:4:"6756";s:11:"res/inc.gif";s:4:"57e4";s:12:"res/java.gif";s:4:"52de";s:11:"res/jpg.gif";s:4:"23ac";s:10:"res/js.gif";s:4:"7a5a";s:16:"res/link_ext.gif";s:4:"03a6";s:16:"res/link_int.gif";s:4:"6e51";s:19:"res/link_mailto.gif";s:4:"c381";s:11:"res/max.gif";s:4:"d6f4";s:11:"res/mid.gif";s:4:"0d95";s:11:"res/mov.gif";s:4:"d5e6";s:11:"res/mp3.gif";s:4:"b37e";s:12:"res/mpeg.gif";s:4:"15b5";s:11:"res/mpg.gif";s:4:"15b5";s:11:"res/pcd.gif";s:4:"e9f3";s:11:"res/pcx.gif";s:4:"7d29";s:11:"res/pdf.gif";s:4:"5c5f";s:12:"res/php3.gif";s:4:"e58b";s:11:"res/png.gif";s:4:"fffc";s:10:"res/ps.gif";s:4:"0b48";s:11:"res/psd.gif";s:4:"4448";s:11:"res/rtf.gif";s:4:"f660";s:12:"res/sgml.gif";s:4:"13e6";s:11:"res/swf.gif";s:4:"d44f";s:11:"res/sxc.gif";s:4:"a29e";s:11:"res/sxw.gif";s:4:"2bc9";s:11:"res/t3d.gif";s:4:"9a71";s:11:"res/t3x.gif";s:4:"558e";s:11:"res/tga.gif";s:4:"20fc";s:11:"res/tif.gif";s:4:"533b";s:12:"res/tmpl.gif";s:4:"5114";s:11:"res/ttf.gif";s:4:"9f93";s:11:"res/txt.gif";s:4:"d7f9";s:11:"res/wav.gif";s:4:"6931";s:11:"res/wrl.gif";s:4:"132d";s:11:"res/xls.gif";s:4:"4a22";s:11:"res/xml.gif";s:4:"2e7b";s:11:"res/xsl.gif";s:4:"2a99";s:11:"res/zip.gif";s:4:"5de4";}',
+	'suggests' => array(
+	),
 );
 
 ?>
