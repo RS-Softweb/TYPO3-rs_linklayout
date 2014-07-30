0b9d7550c5a03bd80bb3924a3782761f51ba670c
renestaeker <renestaeker@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-30 15:23:40 +0000

    Typo3 v6.2 ready and several cleanups (CGL compliance)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rs_linklayout/trunk@86082 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_rslinklayout.php
+++ class.tx_rslinklayout.php
@@ -2,7 +2,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2008-2013 Rene Staeker <typo3@rs-softweb.de>
+ *  (c) 2008-2014 Rene <typo3@rs-softweb.de>
  *  All rights reserved
  *
  *  This script is part of the Typo3 project. The Typo3 project is
@@ -26,31 +26,15 @@
  *
  * $Id$
  *
- * @author Rene Staeker <typo3@rs-softweb.de>
+ * @author Rene <typo3@rs-softweb.de>
  * @package TYPO3
  * @subpackage tx_rslinklayout
  */
-/**
- * [CLASS/FUNCTION INDEX of SCRIPT]
- *
- *
- *
- *   49: class tx_rslinklayout
- *   62:     function main($content, $conf)
- *  111:     function prepare_fileicons($extensions,$filepaths)
- *  158:     function extend_link_params($original, $extension, $delimiter)
- *  203:     function replace_link_params($original, $extension)
- *  271:     function recreate_link($params)
- *
- * TOTAL FUNCTIONS: 5
- * (This index is automatically created/updated by the extension "extdeveval")
- *
- */
 class tx_rslinklayout {
 	// reference to the calling object.
-	var $cObj;
+	public $cObj;
 	// the array with the filetype-image-mapping
-	var $fileicons;
+	private $fileicons;
 
 	/**
 	 * Main function
@@ -59,16 +43,13 @@ class tx_rslinklayout {
 	 * @param	array		TypoScript configuration of the plugin
 	 * @return	string		HTML output
 	 */
-	function main($content, $conf) {
-
-		global $TSFE;
-
+	public function main($content, $conf) {
 		// break if no class is set by TYPO3
 		if (strpos($content['TAG'], 'class=') === FALSE) {
 			return $content['TAG'];
 		}
 
-		if ($conf['linkTargetEnabled'] == True) {
+		if ($conf['linkTargetEnabled'] == TRUE) {
 			if ($content['TYPE'] == 'url') {
 				$linkTargetImg = $this->cObj->IMAGE($conf['linkTargetExt.']);
 				$content['TAG'] = $this->replace_link_params($content['TAG'], $conf['linkTargetExtParams']);
@@ -81,8 +62,8 @@ class tx_rslinklayout {
 			}
 		}
 
-		if ($conf['linkFiletypeEnabled'] == True) {
-			$this->prepare_fileicons($conf['linkFiletypeList'],$conf['linkFiletypePaths']);
+		if ($conf['linkFiletypeEnabled'] == TRUE) {
+			$this->prepare_fileicons($conf['linkFiletypeList'], $conf['linkFiletypePaths']);
 
 			$url = $content['url'];
 
@@ -108,10 +89,10 @@ class tx_rslinklayout {
 	 * @param	string		The comma separated list of search paths for the filetype icons (from TS)
 	 * @return	void
 	 */
-	function prepare_fileicons($extensions,$filepaths) {
-		if ($filepaths)	{
-			$pathArr = t3lib_div::trimExplode(',',$filepaths,1);
-			while(list(,$p)=each($pathArr))	{
+	private function prepare_fileicons($extensions,$filepaths) {
+		if ($filepaths) {
+			$pathArr = t3lib_div::trimExplode(',', $filepaths, 1);
+			while(list(,$p) = each($pathArr)) {
 				if ($p == 'EXT') {
 					$dirArr[] = t3lib_extMgm::siteRelPath('rs_linklayout').'res/';
 				} else {
@@ -123,23 +104,23 @@ class tx_rslinklayout {
 		}
 
 		$this->fileicons = array();
-		$extArr = t3lib_div::trimExplode(',',$extensions,1);
+		$extArr = t3lib_div::trimExplode(',', $extensions, 1);
 
 		foreach($extArr as $valueExt) {
 			reset($dirArr);
-			$found=False;
-			while(list(,$valueDir)=each($dirArr))	{
+			$found=FALSE;
+			while(list(,$valueDir) = each($dirArr)) {
 				if (is_file($valueDir.$valueExt.'.gif')){
 					$this->fileicons[$valueExt] = $valueDir.$valueExt.'.gif';
-					$found = True;
+					$found = TRUE;
 					break;
 				}
 			}
-			if ($found==False) {
+			if ($found==FALSE) {
 				reset($dirArr);
-				while(list(,$valueDir)=each($dirArr))	{
-					if (is_file($valueDir.'default'.'.gif')){
-						$this->fileicons[$valueExt] = $valueDir.'default'.'.gif';
+				while(list(,$valueDir) = each($dirArr)) {
+					if (is_file($valueDir.'default.gif')) {
+						$this->fileicons[$valueExt] = $valueDir.'default.gif';
 						break;
 					}
 				}
@@ -155,7 +136,7 @@ class tx_rslinklayout {
 	 * @param	string		The delimiter
 	 * @return	string		The link as HTML code
 	 */
-	function extend_link_params($original, $extension, $delimiter) {
+	private function extend_link_params($original, $extension, $delimiter) {
 		$originals_temp = array();
 		$originals = array();
 		$extensions_temp = array();
@@ -166,19 +147,22 @@ class tx_rslinklayout {
 		$original = substr($original, strpos($original, '<a ')+3, -1);
 		$original = trim($original, ' "');
 		$originals_temp = explode('" ', $original);
-		for ($i = 0; $i < count($originals_temp); $i++) {
+		$originals_count = count($originals_temp);
+		for ($i = 0; $i < $originals_count; $i++) {
 			$originals[substr($originals_temp[$i], 0, strpos($originals_temp[$i], '="'))] = substr($originals_temp[$i], strpos($originals_temp[$i], '="')+2);
 		}
 
 		$extension = trim($extension);
 		$extensions_temp = explode(' ', $extension);
-		for ($i = 0; $i < count($extensions_temp); $i++) {
+		$extensions_count = count($extensions_temp);
+		for ($i = 0; $i < $extensions_count; $i++) {
 			$extensions[substr($extensions_temp[$i], 0, strpos($extensions_temp[$i], '=')).'_ex'] = substr($extensions_temp[$i], strpos($extensions_temp[$i], '=')+1);
 		}
 
 		$extended_temp = array_merge($originals, $extensions);
+		$extended_count = count($extended_temp);
 		ksort($extended_temp);
-		for ($i = 0; $i < count($extended_temp); $i++) {
+		for ($i = 0; $i < $extended_count; $i++) {
 			$key = key($extended_temp);
 			if ($extended_temp[$key.'_ex'] <> '') {
 				$extended[$key] = $extended_temp[$key].$delimiter.$extended_temp[$key.'_ex'];
@@ -200,7 +184,7 @@ class tx_rslinklayout {
 	 * @param	string		The extension link params
 	 * @return	string		The link as HTML code
 	 */
-	function replace_link_params($original, $extension) {
+	private function replace_link_params($original, $extension) {
 		$originals_temp = array();
 		$originals = array();
 		$extensions_temp = array();
@@ -211,7 +195,8 @@ class tx_rslinklayout {
 		$original = substr($original, strpos($original, '<a ')+3, -1);
 		$original = trim($original, ' "');
 		$originals_temp = explode('" ', $original);
-		for ($i = 0; $i < count($originals_temp); $i++) {
+		$originals_count = count($originals_temp);
+		for ($i = 0; $i < $originals_count; $i++) {
 			$originals[substr($originals_temp[$i], 0, strpos($originals_temp[$i], '="'))] = substr($originals_temp[$i], strpos($originals_temp[$i], '="')+2);
 		}
 
@@ -225,18 +210,21 @@ class tx_rslinklayout {
 		}
 
 		// clean the array values (remove " or ') (thanks "Daniel K.")
-		for ($i = 0; $i < count($extensions_temp); $i++) {
+		$extensions_count = count($extensions_temp);
+		for ($i = 0; $i < $extensions_count; $i++) {
 			$extensions_temp[$i] = str_replace('"', '', $extensions_temp[$i]);
 			$extensions_temp[$i] = str_replace('\'', '', $extensions_temp[$i]);
 		} //end
 
-		for ($i = 0; $i < count($extensions_temp); $i++) {
+		$extensions_count = count($extensions_temp);
+		for ($i = 0; $i < $extensions_count; $i++) {
 			$extensions[substr($extensions_temp[$i], 0, strpos($extensions_temp[$i], '=')).'_ex'] = substr($extensions_temp[$i], strpos($extensions_temp[$i], '=')+1);
 		}
 
 		$extended_temp = array_merge($originals, $extensions);
+		$extended_count = count($extended_temp);
 		ksort($extended_temp);
-		for ($i = 0; $i < count($extended_temp); $i++) {
+		for ($i = 0; $i < $extended_count; $i++) {
 			$key = key($extended_temp);
 			if ($extended_temp[$key.'_ex'] <> '') {
 				$extended[$key] = $extended_temp[$key.'_ex'];
@@ -268,9 +256,10 @@ class tx_rslinklayout {
 	 * @param	array		Array with all link params
 	 * @return	string		The link as HTML code
 	 */
-	function recreate_link($params) {
+	private function recreate_link($params) {
 		$link = '<a ';
-		for ($i = 0; $i < count($params); $i++) {
+		$params_count = count($params);
+		for ($i = 0; $i < $params_count; $i++) {
 			$link .= key($params).'="'.$params[key($params)].'" ';
 			next($params);
 		}
